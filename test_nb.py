e03a1f2b811a0eb4403301e7a8f58a90839ea2f9
Laurent G. OSSBSS AMIS UCC <27735554+LaGuer@users.noreply.github.com>
2019-04-15 03:16:14 -0700

    Update test_nb.py

--- test_nb.py
+++ test_nb.py
@@ -4,46 +4,15 @@ import tempfile
 import sys
 import nbformat
 import doctest
-#import utils
 
-if sys.version_info >= (3,0):
-    kernel = 'python3'
-else:
-    kernel = 'python2'
 
-def _notebook_run(path):
-    """Execute a notebook via nbconvert and collect output.
-       :returns (parsed nb object, execution errors)
-    """
+def _exec_notebook(path):
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
         args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-                "--ExecutePreprocessor.timeout=60",
-                "--ExecutePreprocessor.kernel_name="+kernel,
+                "--ExecutePreprocessor.timeout=1000",
                 "--output", fout.name, path]
         subprocess.check_call(args)
 
-        fout.seek(0)
-        nb = nbformat.reads(fout.read().decode('utf-8'), nbformat.current_nbformat)
 
-    errors = [output for cell in nb.cells if "outputs" in cell
-              for output in cell["outputs"]
-              if output.output_type == "error"]
-
-    return nb, errors
-
-def run_tests():
-#    doctest.testmod()
-
-    # notebooks to ignore in tests:
-    ignore_notebooks = ['at.ipynb']
-
-    for filename in os.listdir('.'):
-        if (filename.split('.')[-1] == 'ipynb' and 
-                    filename not in ignore_notebooks):
-            nb, errors = _notebook_run(filename)
-            if errors != []:
-                raise(Exception)
-
-
-if __name__ == '__main__':
-    run_tests()
+def test():
+    _exec_notebook('test.ipynb')
